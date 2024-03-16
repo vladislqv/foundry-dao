7e0937afd4e919d55e2b7e74868cb65c688dc651
Vladislav Zavtur <ychoeth@proton.me>
2024-03-17 00:45:01 +0200

    governor

---
+++ src/TimeLock.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TimeLock is TimelockController {
+    constructor(uint256 minDelay, address[] memory proposers, address[] memory executors)
+        TimelockController(minDelay, proposers, executors, msg.sender)
+    {}
+}
